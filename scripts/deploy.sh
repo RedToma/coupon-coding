f7f4256f4795424acadb9a07bea6692b5285e3e8
FFG811 <jiminkyu0463@gmail.com>
2024-09-14 21:53:44 +0900

    test-3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,55 +1,53 @@
 #!/bin/bash
 
-# 작업 디렉토리를 /home/ec2-user/app으로 변경
 export TZ="Asia/Seoul"
 cd /home/ec2-user/app
-
-# 환경변수 DOCKER_APP_NAME을 spring으로 설정
 DOCKER_APP_NAME=spring
 
-# 실행중인 blue가 있는지 확인
 EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
+echo "배포 시작 일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
 
-# 배포 시작한 날짜와 시간을 기록
-echo "배포 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
-
-# green이 실행중이면 blue up
 if [ -z "$EXIST_BLUE" ]; then
   echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
-
   sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
-
   sleep 30
 
   BLUE_HEALTH=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 
   if [ -z "$BLUE_HEALTH" ]; then
-    # 슬랙 알림을 보내는 스크립트는 주석 처리
-    # sudo ./slack_blue.sh
-    echo "blue 배포 실패"
+    echo "blue 배포 실패" >> /home/ec2-user/deploy.log
   else
     echo "green 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
     sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
     sudo docker image prune -af
     echo "green 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+
+    sudo sed -i 's/set $active_upstream .*/set $active_upstream blue;/' /etc/nginx/nginx.conf
+    sudo nginx -s reload
+    echo "nginx -> blue 설정 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
   fi
 
 else
-  echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
-  sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
-
+	echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+	sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
   sleep 30
 
   GREEN_HEALTH=$(sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml ps | grep Up)
 
   if [ -z "$GREEN_HEALTH" ]; then
-    # 슬랙 알림을 보내는 스크립트는 주석 처리
-    # sudo ./slack_green.sh
-    echo "green 배포 실패"
+    echo "green 배포 실패" >> /home/ec2-user/deploy.log
   else
     echo "blue 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
     sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
     sudo docker image prune -af
     echo "blue 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+
+    sudo sed -i 's/set $active_upstream .*/set $active_upstream green;/' /etc/nginx/nginx.conf
+    sudo nginx -s reload
+    echo "nginx -> green 설정 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
   fi
 fi
+
+echo "배포 종료  : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ec2-user/deploy.log
+echo "===================== 배포 완료 =====================" >> /home/ec2-user/deploy.log
+echo >> /home/ec2-user/deploy.log
